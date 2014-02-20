14ba9fe81322111e772732db36b92447283d6089
John Obelenus <jobelenus@gmail.com>
2014-02-20 17:15:45 -0500

    ok, going down the ui-router path for this… can always come back to this commit

--- common/static/js/app/app.js
+++ common/static/js/app/app.js
@@ -1,4 +1,21 @@
-var app = angular.module('app', ['appControllers', 'appServices']).run(function($http) {
+var app = angular.module('app', ['appControllers', 'appServices', 'ui.router']).run(function($http) {
     $http.defaults.headers.common.Authorization = 'Basic YWRtaW46cGFzc3dvcmQ=';
     $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+    $stateProvider.state("home", {
+        url: "/",
+        views: {
+            'team': { controller: 'TeamController', templateUrl: '/static/partials/team.view.html' },
+            'player': { controller: 'PlayerController', templateUrl: '/static/partials/player.view.html' },
+            'season': { controller: 'SeasonController', templateUrl: '/static/partials/season.view.html' }
+        }
+    }).state('team.chosen', {
+        url: "/team/:teamId/",
+        controller: function($scope) {
+            console.log('state', $scope);
+        }
+    });
 });
--- common/static/js/app/controller.js
+++ common/static/js/app/controller.js
@@ -1,13 +1,16 @@
 var controllers = angular.module('appControllers', []);
 
-controllers.controller('TeamController', ['$scope', 'Team', function($scope, Team) {
+controllers.controller('TeamController', ['$scope', 'Team', '$state', function($scope, Team, $state) {
     $scope.teams = Team.query();
+    console.log('team', $state);
 }]);
 
-controllers.controller('SeasonController', ['$scope', 'Season', function($scope, Season) {
-        $scope.seasons = Season.query();
+controllers.controller('SeasonController', ['$scope', 'Season', '$state', function($scope, Season, $state) {
+    $scope.seasons = Season.query();
+    console.log('season', $state);
 }]);
 
-controllers.controller('PlayerController', ['$scope', 'Player', function($scope, Player) {
+controllers.controller('PlayerController', ['$scope', 'Player', '$state', function($scope, Player, $state) {
     $scope.players = Player.query();
+    console.log('player', $state);
 }]);
