e5bd9ef8318c6244c0cd36ef622991f5b26265af
John Obelenus <jobelenus@gmail.com>
2014-02-19 22:42:50 -0500

    this is how to do the injection here, and urls ending in /

--- common/static/js/app/services.js
+++ common/static/js/app/services.js
@@ -1,22 +1,22 @@
-var services = angular.module('appServices', ['ngResource']);
+var services = angular.module('appServices', ['djangoRESTResources']);
 
-services.factory('Team', ['$resource',
+services.factory('Team', ['djResource',
   function($resource){
-    return $resource('/rest/common/:teamId', {}, {
+    return $resource('/rest/common/:teamId/', {}, {
       query: {method:'GET', params:{teamId:'teams'}, isArray:true}
     });
   }]);
 
-services.factory('Player', ['$resource',
+services.factory('Player', ['djResource',
   function($resource){
-    return $resource('/rest/common/:playerId', {}, {
+    return $resource('/rest/common/:playerId/', {}, {
       query: {method:'GET', params:{playerId:'players'}, isArray:true}
     });
   }]);
 
-services.factory('Season', ['$resource',
+services.factory('Season', ['djResource',
   function($resource){
-    return $resource('/rest/common/:seasonId', {}, {
+    return $resource('/rest/common/:seasonId/', {}, {
       query: {method:'GET', params:{seasonId:'seasons'}, isArray:true}
     });
   }]);
