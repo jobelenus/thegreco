1921cab06f85c2879e73c1bbbb3d116fbb0fe0b5
John Obelenus <jobelenus@gmail.com>
2014-02-20 19:43:51 -0500

    getting more detail in json output

--- common/rest.py
+++ common/rest.py
@@ -2,18 +2,43 @@ from rest_framework import viewsets, routers, serializers
 import common
 
 
-class SeasonSerializer(serializers.HyperlinkedModelSerializer):
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = common.Team
+        fields = ('id', 'name', 'seasons', 'is_hidden', 'created_on', 'modified_on')
+
+
+class SeasonSerializer(serializers.ModelSerializer):
+    teams = TeamSerializer(source='teams')
+
     class Meta:
         model = common.Season
-        fields = ('id', 'name', 'created_on', 'modified_on')
+        fields = ('id', 'name', 'created_on', 'modified_on', 'teams')
 
+
+class TeamPlayerSeasonSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = common.TeamPlayerSeason
+        fields = ('id', 'team', 'player', 'season', 'created_on', 'modified_on')
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    season_teams = TeamPlayerSeasonSerializer(source='season_teams')
+
+    class Meta:
+        model = common.Player
+        fields = ('id', 'name', 'email', 'gender', 'seasons', 'created_on', 'modified_on', 'season_teams')
+ 
 
 class PlayerViewSet(viewsets.ModelViewSet):
-    model = common.Player
+    queryset = common.Player.objects.all()
+    serializer_class = PlayerSerializer
 
 
 class TeamViewSet(viewsets.ModelViewSet):
-    model = common.Team
+    queryset = common.Team.objects.all()
+    serializer_class = TeamSerializer
 
 
 class SeasonViewSet(viewsets.ModelViewSet):
