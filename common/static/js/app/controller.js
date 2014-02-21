ca8dd7bb53f637fd379c35b5e56bcc7c098787fd
John Obelenus <jobelenus@gmail.com>
2014-02-20 19:45:16 -0500

    teams filter seasons now

--- common/static/js/app/controller.js
+++ common/static/js/app/controller.js
@@ -2,11 +2,28 @@ var controllers = angular.module('appControllers', []);
 
 controllers.controller('TeamController', ['$scope', 'Team', '$state', function($scope, Team, $state) {
     $scope.teams = Team.query();
-    console.log('team', $state);
+    $scope.choose = function(team_id) {
+        if($state.params.player_id) {
+            $state.go('player_team_chosen', {player_id: $state.params.player_id, team_id: team_id});
+        } else if($state.params.season_id) {
+            $state.go('season_team_chosen', {season_id: $state.params.season_id, team_id: team_id});
+        } else {
+            $state.go('team_chosen', {team_id: team_id});
+        }
+    };
 }]);
 
 controllers.controller('SeasonController', ['$scope', 'Season', '$state', function($scope, Season, $state) {
-    $scope.seasons = Season.query();
+    $scope._seasons = Season.query({}, function() {
+        $scope.seasons = $scope._seasons.filter(function(season) {
+            for(var i in season.teams) {
+                if(season.teams[i].id == $state.params.team_id) {
+                    return season;
+                }
+            }
+            return null;
+        });
+    });
     console.log('season', $state);
 }]);
 
