bf4998b3f69ee47f9b485c2c117de350108e4254
John Obelenus <jobelenus@gmail.com>
2014-02-20 19:44:06 -0500

    defining all the states i will need

--- common/static/js/app/app.js
+++ common/static/js/app/app.js
@@ -12,10 +12,28 @@ app.config(function($stateProvider, $urlRouterProvider) {
             'player': { controller: 'PlayerController', templateUrl: '/static/partials/player.view.html' },
             'season': { controller: 'SeasonController', templateUrl: '/static/partials/season.view.html' }
         }
-    }).state('team.chosen', {
-        url: "/team/:teamId/",
-        controller: function($scope) {
-            console.log('state', $scope);
+    });
+    $stateProvider.state('team_chosen', {
+        url: "/team/{team_id}/",
+        views: {
+            'team': { controller: 'TeamController', templateUrl: '/static/partials/team.view.html' },
+            'player': { controller: 'PlayerController', templateUrl: '/static/partials/player.view.html' },
+            'season': { controller: 'SeasonController', templateUrl: '/static/partials/season.view.html' }
         }
+    });
+    $stateProvider.state('season_chosen', {
+        url: "/season/{season_id}/"
+    });
+    $stateProvider.state('player_chosen', {
+        url: "/player/{player_id}/"
+    });
+    $stateProvider.state('season_team_chosen', {
+        url: "/team/{team_id}/season/{season_id}/"
+    });
+    $stateProvider.state('season_player_chosen', {
+        url: "/season/{season_id}/player/{player_id}/"
+    });
+    $stateProvider.state('player_team_chosen', {
+        url: "/player/{player_id}/team/{team_id}/"
     });
 });
